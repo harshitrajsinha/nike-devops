cef1e9898a7ab7afec903942ce9b73740d31fe64
Harshit Raj Sinha <raj.harshitsinha08@gmail.com>
2026-05-27 12:10:38 +0000

    updates

--- src/lib/db/index.ts
+++ src/lib/db/index.ts
@@ -1,10 +1,25 @@
-import { drizzle } from 'drizzle-orm/neon-http';
-import { neon } from '@neondatabase/serverless';
+// import { drizzle } from 'drizzle-orm/neon-http';
+// import { neon } from '@neondatabase/serverless';
+// import * as schema from './schema/index';
+
+// import * as dotenv from 'dotenv';
+
+// dotenv.config({ path: '.env.local' })
+
+// const sql = neon(process.env.DATABASE_URL!);
+// export const db = drizzle(sql, { schema });
+
+
+import { drizzle } from 'drizzle-orm/node-postgres';
+import { Pool } from 'pg';
 import * as schema from './schema/index';
 
 import * as dotenv from 'dotenv';
 
-dotenv.config({ path: '.env.local' })
+dotenv.config({ path: '.env' });
 
-const sql = neon(process.env.DATABASE_URL!);
-export const db = drizzle(sql, { schema });
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+});
+
+export const db = drizzle(pool, { schema });
